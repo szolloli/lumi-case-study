8f9b6f93b73ab5d56bea52246e5274580d602039
szolloli <szolloli@fit.cvut.cz>
2024-06-16 23:54:26 +0200

    add eur formatter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatEur(eur: number) {
+  return eur.toLocaleString("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  });
 }
