0d52532aab585029f217338a98415d8e009da388
szolloli <szolloli@fit.cvut.cz>
2024-06-16 23:58:31 +0200

    update app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import AddSession from "./components/addSession";
+import Sessions from "./components/sessions";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-row items-center justify-between w-full">
+        <h1 className="text-3xl font-bold w-full text-left mb-8">
+          My Sessions
+        </h1>
+        <AddSession />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sessions />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
